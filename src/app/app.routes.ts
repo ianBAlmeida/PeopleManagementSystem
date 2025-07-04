1cef62d43f95f440660e4e3b3c4e5148a9793f4c
ianBAlmeida <bailoneian@gmail.com>
2025-07-03 22:35:06 -0300

    Initial commit - CRUD Pessoa Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ListarPessoaComponent } from './pages/listar-pessoa/listar-pessoa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'pessoas/listar',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pessoas', redirectTo: 'pessoas/listar'
+  },
+  {
+    path: 'pessoas/listar',
+    component: ListarPessoaComponent
+  }
+];
